ff6073f14a9af5920d0da78e0799906fe0f5192e
FredNWorth <paul.shears@rsvp.co.uk>
2023-03-08 17:52:50 +0000

    removed useless useEffect

--- bionicreading/src/bionicwrapper/wrapper.jsx
+++ bionicreading/src/bionicwrapper/wrapper.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import useBionicWrapper from "./useBionicWrapper";
 
 export default function BionicWrapper({ bionicOn, children }) {
   const newChildren = useBionicWrapper(bionicOn, children);
-  useEffect(() => {
 
-  }, [bionicOn])
   return <>{bionicOn ? newChildren : children}</>;
 }
